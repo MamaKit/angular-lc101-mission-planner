c05f504733c7b2b7ae1e565e9f4dc45859ba70c8
MamaKit <mama.kit.pyles@gmail.com>
2022-08-29 20:21:01 -0500

    Completed main Studio. Started Bonus Missions.

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -9,15 +9,16 @@ export class CrewComponent implements OnInit {
 
   inCrew: boolean = false;
   crew: object[] = [];
+  imageView: boolean = false;
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg', dob: "05/26/1971"},
+    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg', dob: "10/17/1956"},
+    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg', dob:"05/10/1958"},
+    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg', dob: "01/07/1941"},
+    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg', dob:"11/22/1942"},
+    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg', dob: "01/23/1973"},
+    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg', dob: "11/03/1970"}
   ];
 
   constructor() { }
@@ -25,5 +26,13 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(candidate) {
+    if(!this.crew.includes(candidate) && this.crew.length < 3) {
+      this.crew.push(candidate);
+    } else if (this.crew.includes(candidate)) {
+      let index = this.crew.indexOf(candidate);
+      this.crew.splice(index,1);
+    }
+  }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
    experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
+   experOnBoard: string[] = [];
+   maxExperiments: number = 4;
 
    constructor() { }
 
    ngOnInit() { }
+
+   addExperiment(experiment) {
+    if(!this.experOnBoard.includes(experiment) && this.experOnBoard.length < this.maxExperiments) {
+      this.experOnBoard.push(experiment);
+    }
+   }
+
+   resetExperiments() {
+    this.experOnBoard = [];
+   }
 
 }
